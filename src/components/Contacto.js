3ae83d31d5fe39e7143240040ba14c1a4897ff47
alexandrajimenezc <artesalexj@gmail.com>
2020-08-31 15:51:50 +0200

    eliminar f.css y hacer resposive contacto

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Contacto.css";
-/* import { Link } from 'react-router-dom' */
+
 
 export default class Contacto extends Component {
   render() {
